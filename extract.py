2cda0fbc4513058ef8613494cb63574cb5666d3e
rj singh <76830320+amrjsingh@users.noreply.github.com>
2023-08-28 20:31:16 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -2,6 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+import pymongo
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# driver = webdriver.Chrome()
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+# import telebot
+from selenium.webdriver.chrome.options import Options
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+uri = os.environ.get('URI')
+chat_id = "6125234965"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+
+
+
 def createDriver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
@@ -14,7 +37,12 @@ def createDriver() -> webdriver.Chrome:
 
     chrome_options.add_experimental_option("prefs", prefs)
     myDriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    msdata = {
+        "chat_id": chat_id,
+        "text": "chromedriver started sucessfully"
+    }
 
+    response = requests.get(url, params=msdata)
     return myDriver
 
 def getGoogleHomepage(driver: webdriver.Chrome) -> str:
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI,Request, BackgroundTasks, HTTPException
 from pydantic import BaseModel
 from extract import *
+from selenium.webdriver.common.keys import Keys
+import os
+import requests
+
+import pymongo
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# driver = webdriver.Chrome()
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+# import telebot
+from selenium.webdriver.chrome.options import Options
 import os
 
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+uri = os.environ.get('URI')
+chat_id = "6125234965"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+
+msdata = {
+    "chat_id": chat_id,
+    "text": "some text"
+}
+
+response = requests.get(url, params=msdata)
+
+
+from appt import *
 SECRET = os.getenv("SECRET")
+driver=createDriver()
+logi=drivelogin(driver)
 
-#
 app = FastAPI()
 
 class Msg(BaseModel):
@@ -16,16 +49,29 @@ class Msg(BaseModel):
 @app.get("/")
 
 async def root():
+    msdata = {
+        "chat_id": chat_id,
+        "text": "bot started"
+    }
+
+    response = requests.get(url, params=msdata)
     return {"message": "Hello World. Welcome to FastAPI!"}
 
 
-@app.get("/homepage")
-async def demo_get():
-    driver=createDriver()
 
-    homepage = getGoogleHomepage(driver)
-    driver.close()
-    return homepage
+
+@app.get("/quiz/{param}")
+async def path_params(param: str):
+    id=param
+    msdata = {
+        "chat_id": chat_id,
+        "text": f"quiz **{id}** scraping started"
+    }
+
+    response = requests.get(url, params=msdata)
+    hj=startB(id,driver)
+
+    return "started plz wait"
 
 @app.post("/backgroundDemo")
 async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
